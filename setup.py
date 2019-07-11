341e5f5541e0fefc2a4e85b0dffaaa1e9f60075f
Mark Vander Stel <mvndrstl@gmail.com>
2019-07-11 19:58:21 -0400

    Add __init__ to plugins package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as fh:
 
 setup(
     name="zeroscale",
-    version="0.1.0",
+    version="0.1.1",
     author="Mark Vander Stel",
     author_email="mvndrstl@gmail.com",
     description="Scale-to-zero any server",
